37a98f705b67952a06d9e9ec8046a69326a495cb
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-04 21:28:40 +0100

    Update TemaDaoJdbc

--- src/main/java/models/daos/jdbc/TemaDaoJdbc.java
+++ src/main/java/models/daos/jdbc/TemaDaoJdbc.java
@@ -4,38 +4,73 @@ import java.util.List;
 
 import models.daos.TemaDao;
 import models.entities.Tema;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TemaDaoJdbc extends GenericDaoJdbc<Tema, Integer> implements TemaDao {
+    private Logger log = LogManager.getLogger(TemaDaoJdbc.class);
+
+    private Tema create(ResultSet resultSet) {
+        try {
+            if (resultSet != null && resultSet.next()) {
+                return new Tema(resultSet.getInt(Tema.ID), resultSet.getString(Tema.NOMBRE),
+                        resultSet.getString(Tema.PREGUNTA));
+            }
+        } catch (SQLException e) {
+            log.error("read: " + e.getMessage());
+        }
+        return null;
+    }
+
+    private static final String SQL_CREATE_TABLE = "CREATE TABLE %s (%s INT NOT NULL, %s VARCHAR(255), "
+            + "%s VARCHAR(255), PRIMARY KEY (%s))";
+
+    public static String sqlToCreateTable() {
+        return String.format(SQL_CREATE_TABLE, Tema.TABLE, Tema.ID, Tema.PREGUNTA, Tema.NOMBRE,
+                Tema.ID);
+    }
+
+    private static final String SQL_INSERT = "INSERT INTO %s (%s,%s,%s) VALUES (%d,'%s','%s')";
 
     @Override
-    public void create(Tema entity) {
-        // TODO Auto-generated method stub
-        
+    public void create(Tema tema) {
+        this.updateSql(String.format(SQL_INSERT, Tema.TABLE, Tema.ID, Tema.PREGUNTA, Tema.NOMBRE,
+                tema.getId(), tema.getPregunta(), tema.getNombre()));
     }
 
     @Override
     public Tema read(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        ResultSet resultSet = this.query(String.format(SQL_SELECT_ID, Tema.TABLE, id));
+        return this.create(resultSet);
     }
 
+    private static final String SQL_UPDATE = "UPDATE %s SET %s='%s', %s='%s' WHERE ID=%d";
+
     @Override
-    public void update(Tema entity) {
-        // TODO Auto-generated method stub
-        
+    public void update(Tema tema) {
+        this.updateSql(String.format(SQL_UPDATE, Tema.TABLE, Tema.NOMBRE, tema.getNombre(),
+                Tema.PREGUNTA, tema.getPregunta(), tema.getId()));
     }
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-        
+        this.updateSql(String.format(SQL_DELETE_ID, Tema.TABLE, id));
     }
 
     @Override
     public List<Tema> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Tema> list = new ArrayList<Tema>();
+        ResultSet resultSet = this.query(String.format(SQL_SELECT_ALL, Tema.TABLE));
+        Tema category = this.create(resultSet);
+        while (category != null) {
+            list.add(category);
+            category = this.create(resultSet);
+        }
+        return list;
     }
-
 
 }
