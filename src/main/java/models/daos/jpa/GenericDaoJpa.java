d3a72056deb76a71c5eb9af0c62e8da60a61bc6e
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-05 21:13:22 +0100

    Update GenericDaoJpa

--- src/main/java/models/daos/jpa/GenericDaoJpa.java
+++ src/main/java/models/daos/jpa/GenericDaoJpa.java
@@ -1,39 +1,97 @@
 package models.daos.jpa;
 
 import java.util.List;
-
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.apache.logging.log4j.LogManager;
 import models.daos.GenericDao;
 
 public class GenericDaoJpa<T, ID> implements GenericDao<T, ID> {
 
+    private Class<T> persistentClass;
+
+    public GenericDaoJpa(Class<T> persistentClass) {
+        this.persistentClass = persistentClass;
+    }
+
     @Override
     public void create(T entity) {
-        // TODO Auto-generated method stub
-        
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(entity);
+            entityManager.getTransaction().commit();
+            LogManager.getLogger(GenericDaoJpa.class).debug("create: " + entity);
+        } catch (Exception e) {
+            LogManager.getLogger(GenericDaoJpa.class).error("create: " + e);
+            if (entityManager.getTransaction().isActive())
+                entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public T read(ID id) {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        T entity = entityManager.find(persistentClass, id);
+        entityManager.close();
+        return entity;
     }
 
     @Override
     public void update(T entity) {
-        // TODO Auto-generated method stub
-        
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.merge(entity);
+            entityManager.getTransaction().commit();
+            LogManager.getLogger(GenericDaoJpa.class).debug("update: " + entity);
+        } catch (Exception e) {
+            LogManager.getLogger(GenericDaoJpa.class).error("update: " + e);
+            if (entityManager.getTransaction().isActive())
+                entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void deleteById(ID id) {
-        // TODO Auto-generated method stub
-        
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        T entity = entityManager.find(persistentClass, id);
+        if (entity != null) {
+            try {
+                entityManager.getTransaction().begin();
+                entityManager.remove(entity);
+                entityManager.getTransaction().commit();
+                LogManager.getLogger(GenericDaoJpa.class).debug("delete: " + entity);
+            } catch (Exception e) {
+                LogManager.getLogger(GenericDaoJpa.class).error("delete: " + e);
+                if (entityManager.getTransaction().isActive())
+                    entityManager.getTransaction().rollback();
+            } finally {
+                entityManager.close();
+            }
+        }
     }
 
     @Override
     public List<T> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(this.persistentClass);
+        Root<T> root = criteriaQuery.from(this.persistentClass);
+        criteriaQuery.select(root); 
+        TypedQuery<T> typedQuery = entityManager.createQuery(criteriaQuery);
+        typedQuery.setFirstResult(0); 
+        typedQuery.setMaxResults(0); 
+        List<T> result = typedQuery.getResultList();
+        entityManager.close();
+        return result;
     }
 
 }
