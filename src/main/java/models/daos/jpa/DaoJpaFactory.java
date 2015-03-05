50a18bfed09c74b9df871139380ea89d5f489238
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-05 13:10:28 +0100

    Update DaoJpaFactory

--- src/main/java/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/models/daos/jpa/DaoJpaFactory.java
@@ -1,15 +1,32 @@
 package models.daos.jpa;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+
 import models.daos.DaoFactory;
 import models.daos.TemaDao;
 
 
 public class DaoJpaFactory extends DaoFactory{
 
+private static final String PERSISTENCE_UNIT = "JEELuisQuishpi";
+    
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
+
+    public DaoJpaFactory() {
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+       
     @Override
     public TemaDao getTemaDao() {
-        // TODO Auto-generated method stub
-        return null;
+        return new TemaDaoJpa();
     }
 
     
