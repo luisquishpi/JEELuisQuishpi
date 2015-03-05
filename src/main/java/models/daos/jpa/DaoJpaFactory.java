a416d1e808a80c3c0725bd27a6b1841e3d6f175a
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-05 12:55:55 +0100

    Update DaoJpaFactory

--- src/main/java/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/models/daos/jpa/DaoJpaFactory.java
@@ -1,39 +1,17 @@
 package models.daos.jpa;
 
-import java.util.List;
+import models.daos.DaoFactory;
+import models.daos.TemaDao;
 
-import models.daos.GenericDao;
 
-public class DaoJpaFactory<T, ID> implements GenericDao<T, ID>{
-
-    @Override
-    public void create(T entity) {
-        // TODO Auto-generated method stub
-        
-    }
+public class DaoJpaFactory extends DaoFactory{
 
     @Override
-    public T read(ID id) {
+    public TemaDao getTemaDao() {
         // TODO Auto-generated method stub
         return null;
     }
 
-    @Override
-    public void update(T entity) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void deleteById(ID id) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public List<T> findAll() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    
 
 }
