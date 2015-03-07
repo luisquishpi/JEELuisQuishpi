b706a5b8cf5134d87e8ef923245989cee231fce5
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-07 19:21:50 +0100

    INIT - Nuevo proyecto para hacer pruebas JPA

--- src/main/java/models/entities/Tema.java
+++ src/main/java/models/entities/Tema.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
+
 @Entity
 public class Tema {
     public static final String TABLE = "tema";
