4404f68e6fd425dafe71fb62dc2a3d3e43c69d75
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-09 00:36:48 +0100

    Update TemaDaoJpaTest, OK!

--- src/test/java/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/models/daos/jpa/TemaDaoJpaTest.java
@@ -1,14 +1,65 @@
 package models.daos.jpa;
 
 import static org.junit.Assert.*;
+import models.daos.DaoFactory;
+import models.daos.TemaDao;
+import models.entities.Tema;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import persistence.jpa.JpaFactory;
+
 public class TemaDaoJpaTest {
+    private TemaDao dao;
+
+    private Tema tema;
+
+    @BeforeClass
+    public static void beforeClass() {
+        DaoFactory.setFactory(new DaoJpaFactory());
+        JpaFactory.dropAndCreateTables();
+        System.out.println("id=" + JpaFactory.getEntityManagerFactory());
+    }
+
+    @Before
+    public void before() {
+        this.tema = new Tema("Sistemas", "Pregunta1");
+        dao = DaoFactory.getFactory().getTemaDao();
+        dao.create(tema);
+    }
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testRead() {
+        System.out.println("id=" + tema.getId());
+        assertEquals(tema, dao.read(tema.getId()));
+    }
+
+    @Test
+    public void testUpdateCategory() {
+        tema.setNombre("other");
+        tema.setPregunta("other");
+        dao.update(tema);
+    }
+
+    @Test
+    public void testDeleteByID() {
+        dao.deleteById(tema.getId());
+        assertNull(dao.read(tema.getId()));
+    }
+
+    @Test
+    public void testFindAll() {
+        dao.create(new Tema("2", "2"));
+        dao.create(new Tema("3", "3"));
+        assertEquals(3, dao.findAll().size());
+    }
+
+    @After
+    public void after() {
+        JpaFactory.dropAndCreateTables();
     }
 
 }
