0834b3eff503f68a60b4bfd5b563bd4163eca0a6
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-08 23:18:26 +0100

    Add TemaDaoJpaTest

---
+++ src/test/java/models/daos/jpa/TemaDaoJpaTest.java
@@ -0,0 +1,14 @@
+package models.daos.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemaDaoJpaTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
