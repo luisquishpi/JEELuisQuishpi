0da70cfe90c8720dded68c9d05de1207f99dd4f4
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-09 20:08:35 +0100

    Add - VotoDaoJpaTest

---
+++ src/test/java/models/daos/jpa/VotoDaoJpaTest.java
@@ -0,0 +1,14 @@
+package models.daos.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VotoDaoJpaTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
