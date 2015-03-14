a8a0c5ba91e39af518b4613a1814302a514e617b
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-15 00:08:05 +0100

    Update VotoDaoJpa, métodos para recuperar listas de: número de votos y promedio segun nivel de estudio

--- src/main/java/models/daos/jpa/VotoDaoJpa.java
+++ src/main/java/models/daos/jpa/VotoDaoJpa.java
@@ -1,13 +1,31 @@
 package models.daos.jpa;
 
+import java.util.List;
 
+import javax.persistence.Query;
+
+import persistence.jpa.JpaFactory;
 import models.daos.VotoDao;
 import models.entities.Voto;
 
-public class VotoDaoJpa extends GenericDaoJpa<Voto, Integer> implements VotoDao{
+public class VotoDaoJpa extends GenericDaoJpa<Voto, Integer> implements VotoDao {
+
+    private static Query query;
 
     public VotoDaoJpa() {
         super(Voto.class);
     }
 
+    @SuppressWarnings("unchecked")
+    public static List<Object[]> findNumeroVotos() {
+        query = JpaFactory.getEntityManagerFactory().createEntityManager()
+                .createNamedQuery(Voto.FIND_NUMERO_VOTOS);
+        return query.getResultList();
+    }
+    @SuppressWarnings("unchecked")
+    public static List<Object[]> findVotacionMedia() {
+        query = JpaFactory.getEntityManagerFactory().createEntityManager()
+                .createNamedQuery(Voto.FIND_VOTACION_MEDIA);
+        return  query.getResultList();
+    }
 }
