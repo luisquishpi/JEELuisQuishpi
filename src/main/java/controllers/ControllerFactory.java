c6dd21889a7df4859773db0c0905e397c33e0da3
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-15 22:44:52 +0100

    Add ControllerFactory

---
+++ src/main/java/controllers/ControllerFactory.java
@@ -0,0 +1,22 @@
+package controllers;
+
+
+
+public abstract class ControllerFactory {
+    public static ControllerFactory factory = null;
+
+    public static void setFactory(ControllerFactory factory) {
+        ControllerFactory.factory = factory;
+    }
+
+    public static ControllerFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public abstract VotarController getVotarCotroller();
+    public abstract VerVotacionesController getVerVotacionesController();
+    public abstract NuevoTemaCotroller getNuevoTemaController();
+    public abstract EliminarTemaController getEliminarTemaController();   
+
+}
