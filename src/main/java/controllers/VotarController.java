9857b611c0d8d3f9c205b8bbce0d26397097e9ce
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-15 23:37:09 +0100

    Update VotarController, se agrega nuevos métodos

--- src/main/java/controllers/VotarController.java
+++ src/main/java/controllers/VotarController.java
@@ -1,10 +1,21 @@
 package controllers;
 
+import java.util.List;
+
 import models.entities.Tema;
 import models.entities.Voto;
+import models.utils.NivelEstudio;
 
 public interface VotarController {
 
-    void SaveVoto(Tema tema,Voto voto);
+    void SaveVoto(Tema tema, Voto voto);
+
+    List<Tema> listTema();
+
+    String getPregunta();
+
+    List<NivelEstudio> listNivelEstudio();
+
+    List<Integer> listValorVoto();
 
 }
