ac942d96926deeba1af72549713a9b76a2971c89
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-16 15:31:43 +0100

    Update controllers.ejb

--- src/main/java/controlles/ejb/ControllerEjbFactory.java
+++ src/main/java/controlles/ejb/ControllerEjbFactory.java
@@ -2,31 +2,38 @@ package controlles.ejb;
 
 import controllers.ControllerFactory;
 import controllers.EliminarTemaController;
-import controllers.NuevoTemaCotroller;
+import controllers.NuevoTemaController;
 import controllers.VerVotacionesController;
 import controllers.VotarController;
 
 public class ControllerEjbFactory extends ControllerFactory {
 
     private VotarController votarController;
+
+    private NuevoTemaController nuevoTemaController;
+
     @Override
     public VotarController getVotarCotroller() {
-        if(votarController==null) votarController=new VotarCotrollerEjb();
+        if (votarController == null)
+            votarController = new VotarCotrollerEjb();
         return votarController;
     }
 
     @Override
-    public VerVotacionesController getVerVotacionesController() {
-        return null;
-    }
-
-    @Override
-    public NuevoTemaCotroller getNuevoTemaController() {
-        return null;
+    public NuevoTemaController getNuevoTemaController() {
+        if (nuevoTemaController == null)
+            nuevoTemaController = new NuevoTemaCotrollerEjb();
+        return nuevoTemaController;
     }
 
     @Override
     public EliminarTemaController getEliminarTemaController() {
+        return null;
+    }
+
+    @Override
+    public VerVotacionesController getVerVotacionesController() {
+        // TODO Auto-generated method stub
         return null;
     }
 
--- src/main/java/controlles/ejb/VotarCotrollerEjb.java
+++ src/main/java/controlles/ejb/VotarCotrollerEjb.java
@@ -5,20 +5,24 @@ import java.util.List;
 import models.daos.DaoFactory;
 import models.daos.TemaDao;
 import models.daos.VotoDao;
+import models.daos.jpa.TemaDaoJpa;
 import models.entities.Tema;
 import models.entities.Voto;
-import models.utils.NivelEstudio;
 import controllers.VotarController;
 
 public class VotarCotrollerEjb implements VotarController {
+
+    List<Integer> valores;
+
+    private TemaDao temaDao;
+
+    private VotoDao votoDao;
 
     public VotarCotrollerEjb() {
         temaDao = DaoFactory.getFactory().getTemaDao();
         votoDao = DaoFactory.getFactory().getVotoDao();
     }
 
-    private TemaDao temaDao;
-    private VotoDao votoDao;
     @Override
     public void SaveVoto(Tema tema, Voto voto) {
         temaDao.create(tema);
@@ -27,22 +31,20 @@ public class VotarCotrollerEjb implements VotarController {
 
     @Override
     public List<Tema> listTema() {
-        return null;
-    }
-
-    @Override
-    public String getPregunta() {
-        return null;
-    }
-
-    @Override
-    public List<NivelEstudio> listNivelEstudio() {
-        return null;
+        return temaDao.findAll();
     }
 
     @Override
     public List<Integer> listValorVoto() {
-        return null;
+        for (int i = 0; i < 10; i++) {
+            valores.add(i);
+        }
+        return valores;
+    }
+
+    @Override
+    public String getPregunta(Tema tema) {
+        return TemaDaoJpa.findById(tema.getId()).getPregunta();
     }
 
 }
