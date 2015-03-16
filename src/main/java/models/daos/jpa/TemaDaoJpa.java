0ff7871b8b05bf6ff80246c350879f05794f0625
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-16 02:13:04 +0100

    Update TemaDaoJpa model, add findById method

--- src/main/java/models/daos/jpa/TemaDaoJpa.java
+++ src/main/java/models/daos/jpa/TemaDaoJpa.java
@@ -1,12 +1,22 @@
 package models.daos.jpa;
 
+import javax.persistence.EntityManager;
+
 import models.daos.TemaDao;
 import models.entities.Tema;
 
 public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao {
 
+    private Class<Tema> persistentClass;
+
     public TemaDaoJpa() {
         super(Tema.class);
+    }
+
+    public Tema findById(int id) {
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        Tema entity = entityManager.find(persistentClass, id);
+        return entity;
     }
 
 }
