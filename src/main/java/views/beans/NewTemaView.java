0890c561ed717a67de29b31bce2ec8390c100bc8
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-16 17:32:41 +0100

    Update: ghange TemaView to NewTemaView

--- src/main/java/views/beans/NewTemaView.java
+++ src/main/java/views/beans/NewTemaView.java
@@ -1,28 +1,28 @@
 package views.beans;
 
-import org.apache.logging.log4j.LogManager;
-
 import models.daos.DaoFactory;
 import models.daos.TemaDao;
 import models.daos.jpa.DaoJpaFactory;
 import models.entities.Tema;
 
-public class TemaView {
+public class NewTemaView {
     private Tema tema;
+
     private TemaDao temaDao;
-    
-    public TemaView(){
-        
+
+    public NewTemaView() {
+
     }
+
     public void setTema(Tema tema) {
-        this.tema=tema;
+        this.tema = tema;
     }
-    public Tema getTema(){
-       return tema;
+
+    public Tema getTema() {
+        return tema;
     }
+
     public String process() {
-        LogManager.getLogger(TemaView.class).debug(
-                    "Se accede a la capa de negocio para registrar persona: " + tema);
         DaoFactory.setFactory(new DaoJpaFactory());
         temaDao = DaoFactory.getFactory().getTemaDao();
         temaDao.create(tema);
