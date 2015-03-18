4396175f1ed9719694d4bc1bec81bf40cd5260e1
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-18 10:58:50 +0100

    Add ReportControllerEjb

---
+++ src/main/java/controlles/ejb/ReportCotrollerEjb.java
@@ -0,0 +1,19 @@
+package controlles.ejb;
+
+import java.util.List;
+
+import controllers.ReportController;
+
+public class ReportCotrollerEjb implements ReportController {
+
+    @Override
+    public List<Object[]> listNumeroVotos() {
+        return null;
+    }
+
+    @Override
+    public List<Object[]> listVotacionMedia() {
+        return null;
+    }
+
+}
