5f5a7c336ffa7c28bc5b73ab8e8f541a44f5e5b3
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-24 21:13:19 +0100

    Add TemaSourceTest

---
+++ src/test/java/ws/TemaSourceTest.java
@@ -0,0 +1,14 @@
+package ws;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TemaSourceTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
