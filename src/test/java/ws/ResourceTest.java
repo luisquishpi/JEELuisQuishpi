ec0dc2cfd73a8073d0502e0951689720cce55e2c
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-24 21:16:40 +0100

    Add TemaSourceTest

---
+++ src/test/java/ws/ResourceTest.java
@@ -0,0 +1,14 @@
+package ws;
+
+public class ResourceTest {
+
+    private static final String PROTOCOL = "http";
+
+    private static final String HOST = "localhost";
+
+    private static final int PORT = 8080;
+
+    private static final String WEB = "/web/rest";
+
+    protected static final String URI = PROTOCOL + "://" + HOST + ":" + PORT + WEB;
+}
