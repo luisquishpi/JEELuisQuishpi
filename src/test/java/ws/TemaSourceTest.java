6eacf7faf3700cdca928997d0c91d987f3346af8
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-25 23:58:17 +0100

    Update TemaSourceTest.java, createTest() Ok

--- src/test/java/ws/TemaSourceTest.java
+++ src/test/java/ws/TemaSourceTest.java
@@ -1,14 +1,28 @@
 package ws;
 
 import static org.junit.Assert.*;
+import models.entities.Tema;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class TemaSourceTest {
+import controllers.ws.WsManager;
 
+public class TemaSourceTest extends ResourceTest {
+
+    private Tema tema1;
+    WsManager wsManager;
+    
+
+    @Before
+    public void before() {
+        this.tema1 = new Tema("Tema test 1", "Pregunta del tema test 1");
+        
+    }
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void createTest() {
+        wsManager=new WsManager(URI, TemaUris.PATH_TEMAS);
+        assertTrue(wsManager.create(this.tema1));
     }
 
 }
