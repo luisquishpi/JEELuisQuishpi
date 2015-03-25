5de28fc71b12e605794e441f43652e3200497643
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-26 00:00:32 +0100

    Update REST TemaSourcesTest, createTest() Ok

--- src/test/java/ws/TemaSourceTest.java
+++ src/test/java/ws/TemaSourceTest.java
@@ -11,17 +11,18 @@ import controllers.ws.WsManager;
 public class TemaSourceTest extends ResourceTest {
 
     private Tema tema1;
+
     WsManager wsManager;
-    
 
     @Before
     public void before() {
         this.tema1 = new Tema("Tema test 1", "Pregunta del tema test 1");
-        
+
     }
+
     @Test
     public void createTest() {
-        wsManager=new WsManager(URI, TemaUris.PATH_TEMAS);
+        wsManager = new WsManager(URI, TemaUris.PATH_TEMAS);
         assertTrue(wsManager.create(this.tema1));
     }
 
