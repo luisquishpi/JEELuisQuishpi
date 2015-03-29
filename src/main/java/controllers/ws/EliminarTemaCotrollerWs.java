849d8d549c9b4bd4dde1b7429ae5a86f7365cf4f
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-29 03:13:28 +0200

    Update EliminarTemaControllerWs

--- src/main/java/controllers/ws/EliminarTemaCotrollerWs.java
+++ src/main/java/controllers/ws/EliminarTemaCotrollerWs.java
@@ -2,20 +2,24 @@ package controllers.ws;
 
 import java.util.List;
 
+import models.daos.DaoFactory;
+import models.daos.TemaDao;
 import models.entities.Tema;
 import controllers.EliminarTemaController;
 
 public class EliminarTemaCotrollerWs extends ControllerWs implements EliminarTemaController {
 
+    private TemaDao temaDao;
+
     @Override
     public void removeTema(Tema tema) {
-        // TODO Auto-generated method stub
-
+        temaDao = DaoFactory.getFactory().getTemaDao();
+        temaDao.deleteById(tema.getId());
     }
 
     @Override
     public List<Tema> listaTemas() {
-        // TODO Auto-generated method stub
+        temaDao.findAll();
         return null;
     }
 
