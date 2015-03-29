ab98082f3fdc6e582a035a15da579bb41fcadeb2
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-29 01:57:42 +0100

    Add VotoUris

---
+++ src/main/java/ws/VotoUris.java
@@ -0,0 +1,11 @@
+package ws;
+
+public interface VotoUris {
+
+    String PATH_VOTAR = "/votar";
+
+    String PATH_VERTOTALVOTOS = "/vertotalvotos";
+
+    String PATH_VERPROMEDIOVOTOS = "/verpromediovotos";
+
+}
