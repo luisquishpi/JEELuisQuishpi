190acc4c77e020aaa6c59906d1c2280a9ad06e28
Luis Quishpi <luisquishpi @138.4.205.235>
2015-03-29 03:11:04 +0200

    Update TemaSourceTest.java

--- src/test/java/ws/TemaSourceTest.java
+++ src/test/java/ws/TemaSourceTest.java
@@ -2,31 +2,29 @@ package ws;
 
 import static org.junit.Assert.*;
 import models.entities.Tema;
-
-import org.apache.logging.log4j.LogManager;
 import org.junit.Before;
 import org.junit.Test;
-
 import controllers.ws.WsManager;
 
 public class TemaSourceTest extends ResourceTest {
 
     private Tema tema1;
 
+    private Tema tema2;
+
     WsManager wsManager;
 
     @Before
     public void before() {
         this.tema1 = new Tema("Tema test 1", "Pregunta del tema test 1");
-
+        new WsManager(URI, TemaUris.PATH_TEMAS).create(this.tema1);
     }
 
     @Test
     public void createTest() {
-        LogManager.getLogger(this.getClass()).debug(TemaUris.PATH_TEMAS);
+        this.tema2 = new Tema("Tema test 2", "Pregunta del tema test 2");
         wsManager = new WsManager(URI, TemaUris.PATH_TEMAS);
-        assertTrue(wsManager.create(this.tema1));
-        
+        assertTrue(wsManager.create(this.tema2));
     }
 
 }
